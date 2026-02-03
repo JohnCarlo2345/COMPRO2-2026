0de865d6ede427ec8449adcafdbba08488237014
JohnCarlo2345 <johncarlo.valdez@lorma.edu>
2026-02-03 12:18:13 +0800

    submit activity

---
+++ activity4/CvsApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CvsApp {
+    static String[] name;
+    static double[][] grade;
+
+    public static void main(String[] args) {
+        name = new String[50];
+        grade = new double[50][2];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int r = 0; r < 3; r++) {
+            System.out.print("Enter subject: ");
+            
+        }
+    }
+}
