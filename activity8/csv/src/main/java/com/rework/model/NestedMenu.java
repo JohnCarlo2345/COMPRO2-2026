d361dc77da5ac29d6d66798b7ef757703c3395c6
Nagabtac <darwin.catbagan@lorma.edu>
2026-03-16 12:12:45 +0800

    submit activity

---
+++ activity8/csv/src/main/java/com/rework/model/NestedMenu.java
@@ -0,0 +1,24 @@
+package com.rework.model;
+
+import java.util.Scanner;
+import org.json.JSONObject;
+import java.io.FileWriter;
+
+public class NestedMenu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[] subjectGrades = new double[3];
+        String[] subjects = {"COMPRO2", "DSA", "OOP"};
+        int mainChoice, subChoice;
+        final String JSON_FILE = "gradesData.json";
+
+        loadGrades(subjectGrades, subjects, JSON_FILE);
+
+        do { 
+            System.out.println("MAIN MENU:");
+            System.out.println("[1] Enter Grades");
+            System.out.println("[2] Display Grades");
+            System.out.println("[3] Exit");
+            System.out.print("Enter choice");
+            mainChoice = scanner.nextInt();
+        }
